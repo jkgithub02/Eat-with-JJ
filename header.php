a497a0a336b898ee0676ddea4345404f7aa3e535
jkgithub02 <148358604+jkgithub02@users.noreply.github.com>
2024-03-30 22:16:35 +0800

    Add files via upload

--- header.php
+++ header.php
@@ -1,11 +1,16 @@
+
 <header>
     <h1>Eat with JJ</h1>
     <nav>
       <a href="home.php" class="button-link">Home</a>
       <a href="menu.php" class="button-link">Menu</a>
       <a href="about.php" class="button-link">About Us</a>
-      <a href="login.php" class="button-link">Login</a>
-      <a href="signup.php" class="button-link">Sign Up</a>
+      <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+        <a href="logout.php" class="button-link">Logout</a>
+      <?php else: ?>
+        <a href="login.php" class="button-link">Login</a>
+        <a href="signup.php" class="button-link">Sign Up</a>
+      <?php endif; ?>
       <a href="cart.php"> <i class="fas fa-shopping-cart"></i> </a>
       </nav>
 </header>
